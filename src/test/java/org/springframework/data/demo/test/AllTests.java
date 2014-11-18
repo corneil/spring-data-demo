c94d4674d17ca07a5db28347394cef6b1c305440
Corneil du Plessis <corneil.duplessis@gmail.com>
2014-11-18 16:02:04 +0200

    Added AllTests JUnit Test Suite Added launchers for STS for build and test suite. Removed launchers to individial tests.

---
+++ src/test/java/org/springframework/data/demo/test/AllTests.java
@@ -0,0 +1,12 @@
+package org.springframework.data.demo.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AuditEntryTest.class, LocationUpdateTest.class,
+		UserGroupDataServiceIntegrationTest.class })
+public class AllTests {
+
+}
